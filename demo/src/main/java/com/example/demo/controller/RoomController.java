e5200beafaefcee626ea73ebdf13635d36fb2427
parkchanmin <parkchanmin990804@naver.com>
2024-12-12 19:02:44 +0900

    12.12 chat User modify and currentUser modify

--- demo/src/main/java/com/example/demo/controller/RoomController.java
+++ demo/src/main/java/com/example/demo/controller/RoomController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -47,4 +48,18 @@ public class RoomController {
         mv.addObject("roomId", roomId);
         return mv;
     }
+
+
+    // @GetMapping("/{roomId}/check")
+    // @ResponseBody
+    // public ResponseEntity<?> checkRoomAvailability(@PathVariable String roomId) {
+    //     ChatRoom room = chatRoomService.getRoom(roomId);
+    //     if (room == null) {
+    //         return ResponseEntity.notFound().build();
+    //     }
+    //     if (room.isFull()) {
+    //         return ResponseEntity.badRequest().body("Room is full");
+    //     }
+    //     return ResponseEntity.ok().build();
+    // }
 }
