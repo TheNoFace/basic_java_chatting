d6208a863696a29cd3ad5a886fd17eb41b5ae7c5
pcmin99 <parkchanmin990804@naver.com>
2025-03-05 17:24:49 +0900

    new computer project save

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.model.ChatRoom;
 import com.example.demo.service.ChatService;
 import com.example.demo.service.UserService;
 
--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,13 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,7 +18,6 @@ import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.ChatService;
 import com.example.demo.service.UserService;
 
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
